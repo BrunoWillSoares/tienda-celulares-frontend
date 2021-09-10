a5e95e41100fcde37fe9da53da3c9d654c6ad7a1
BrunoWillSoares <bruno.p.soares@gmail.com>
2021-09-10 14:03:00 -0300

    Heroku22

--- server.js
+++ server.js
@@ -7,9 +7,17 @@ const app = express();
 //Serve only the static files from the dist directory
 app.use(express.static('./dist/tienda-celulares-frontendapp'));
 
-app.get('/*', function (req, res) { 
+app.get('/*', function (req, res, next) {
+    var options = {
+      root: path.join(__dirname, 'public'),
+      dotfiles: 'deny',
+      headers: {
+        'x-timestamp': Date.now(),
+        'x-sent': true
+      }
+    }
         var fileName = req.params.name;
-        res.sendFile(fileName, {root: 'dist/angular-heroku/'})
+        res.sendFile(fileName, options)
     },
 );
 
