965591cd569f7936e368747e962e05cc956cec20
BrunoWillSoares <bruno.p.soares@gmail.com>
2021-09-10 15:31:24 -0300

    Heroku21

--- server.js
+++ server.js
@@ -7,29 +7,6 @@ const app = express();
 //Serve only the static files from the dist directory
 app.use(express.static('./dist/tienda-celulares-frontendapp'));
 
-app.get('/*', function (req, res, next) {
-    var options = {
-      root: path.join(__dirname, 'public'),
-      dotfiles: 'deny',
-      headers: {
-        'x-timestamp': Date.now(),
-        'x-sent': true
-      }
-    }
-        var fileName = req.params.name;
-        console.log('Dir: ' + __dirname);
-        console.log('BaseURL: ' + req.baseUrl);
-        console.log('OriginalURL: ' + req.originalUrl);
-        console.log('Path1: ' + req.path);
-        console.log('Path2: ' + path.join(__dirname, 'public'));
-        console.log('Entrada: ' + req);
-        console.log('Entrada2: ' + req.params);
-        console.log('Entrada3: ' + req.params[0]);
-        console.log('Arquivo: ' + fileName);
-        console.log('Arquivo Com path: ' + path.join(fileName) + ' - ' + path.resolve(fileName));
-        res.sendFile(path.join(fileName), options)
-    },
-);
 
 
 
