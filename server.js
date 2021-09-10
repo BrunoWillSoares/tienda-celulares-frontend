68081e09964f72a37be2819788a2bfc112637b1a
BrunoWillSoares <bruno.p.soares@gmail.com>
2021-09-10 13:23:13 -0300

    Configuracao

--- server.js
+++ server.js
@@ -7,33 +7,41 @@ const app = express();
 //Serve only the static files from the dist directory
 app.use(express.static('./dist/tienda-celulares-frontendapp'));
 
-app.use('/Success', express.static('./dist/tienda-celulares-frontendapp/success.html'));
+//app.use('/Success', express.static('./dist/tienda-celulares-frontendapp/success.html'));
 
-app.use('/success.html', express.static('./dist/tienda-celulares-frontendapp/success.html'));
+//app.use('/success.html', express.static('./dist/tienda-celulares-frontendapp/success.html'));
 
 app.get('/',(req, res) => 
-    res.sendFile('index.html', {root: 'dist/angula-heroku/'}),
+    res.sendFile('index.html', {root: 'dist/angular-heroku/'}),
 );
 
 app.get('Success',(req, res) => 
-    res.sendFile('success.html', {root: 'dist/angula-heroku/'}),
+    res.sendFile('success.html', {root: 'dist/angular-heroku/'}),
+);
+
+app.get('/Success',(req, res) => 
+    res.sendFile('./success.html', {root: 'dist/angular-heroku/'}),
 );
 
 app.get('success.html',(req, res) => 
-    res.sendFile('success.html', {root: 'dist/angula-heroku/'}),
+    res.sendFile('./success.html', {root: 'dist/angular-heroku/'}),
+);
+app.get('/success.html',(req, res) => 
+    res.sendFile('./success.html', {root: 'dist/angular-heroku/'}),
 );
 
+
 app.get('/index.html',(req, res) => 
-    res.sendFile('index.html', {root: 'dist/angula-heroku/'}),
+    res.sendFile('index.html', {root: 'dist/angular-heroku/'}),
 );
 
 
 app.get('/Fail',(req, res) => 
-    res.sendFile('fail.html', {root: 'dist/angula-heroku/'}),
+    res.sendFile('fail.html', {root: 'dist/angular-heroku/'}),
 );
 
 app.get('/Pending',(req, res) => 
-    res.sendFile('pending.html', {root: 'dist/angula-heroku/'}),
+    res.sendFile('pending.html', {root: 'dist/angular-heroku/'}),
 );
 
 
