cb93e019a23f8bc187a583868af6e2e2055903ec
BrunoWillSoares <bruno.p.soares@gmail.com>
2021-09-10 12:38:31 -0300

    Roteamento

--- server.js
+++ server.js
@@ -7,13 +7,22 @@ const app = express();
 //Serve only the static files from the dist directory
 app.use(express.static('./dist/tienda-celulares-frontendapp'));
 
-app.get('/*',(req, res) => 
+app.get('/',(req, res) => 
     res.sendFile('index.html', {root: 'dist/angula-heroku/'}),
 );
 
 app.get('/Success',(req, res) => 
     res.sendFile('success.html', {root: 'dist/angula-heroku/'}),
 );
+
+app.get('/success.html',(req, res) => 
+    res.sendFile('success.html', {root: 'dist/angula-heroku/'}),
+);
+
+app.get('/index.html',(req, res) => 
+    res.sendFile('index.html', {root: 'dist/angula-heroku/'}),
+);
+
 
 app.get('/Fail',(req, res) => 
     res.sendFile('fail.html', {root: 'dist/angula-heroku/'}),
