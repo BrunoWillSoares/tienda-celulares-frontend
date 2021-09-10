4e2ca1be87d84b623174bbf9199d0147ae8e9865
BrunoWillSoares <bruno.p.soares@gmail.com>
2021-09-10 20:06:50 -0300

    Heroku 14

--- server.js
+++ server.js
@@ -9,35 +9,6 @@ app.use(express.static('./dist/tienda-celulares-frontendapp'));
 app.use('/Success',express.static('./dist/tienda-celulares-frontendapp/'));
 app.use('/Fail',express.static('./dist/tienda-celulares-frontendapp/'));
 app.use('/Pending',express.static('./dist/tienda-celulares-frontendapp/'));
-app.use('/success.html',express.static('./dist/tienda-celulares-frontendapp/'));
-
-/*
-app.get('/*', function (req, res, next) {
-    var options = {
-      root: path.join(__dirname, 'public'),
-      dotfiles: 'deny',
-      headers: {
-        'x-timestamp': Date.now(),
-        'x-sent': true
-      }
-    }
-        var fileName = req.params.name;
-        console.log('Dir: ' + __dirname);
-        console.log('BaseURL: ' + req.baseUrl);
-        console.log('OriginalURL: ' + req.originalUrl);
-        console.log('Path1: ' + req.path);
-        console.log('Path2: ' + path.join(__dirname, '../public'));
-        console.log('Entrada: ' + req);
-        console.log('Entrada2: ' + req.params);
-        console.log('Entrada3: ' + req.params[0]);
-        console.log('Arquivo: ' + fileName);
-        console.log('Arquivo Com path: ' + path.join(fileName) + ' - ' + path.resolve(fileName));
-        res.sendFile(path.join(fileName), options)
-    },
-);
-
-*/
-
 
 //Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
