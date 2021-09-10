a1e14abbcf0ba445328d8e44c5023620a8293572
BrunoWillSoares <bruno.p.soares@gmail.com>
2021-09-10 13:43:33 -0300

    Heroku not Serving

--- server.js
+++ server.js
@@ -2,40 +2,30 @@
 const express = require('express');
 const path = require('path');
 
+var options = {
+    dotfiles: 'ignore',
+    etag: false,
+    extensions: ['htm', 'html'],
+    index: false,
+    maxAge: '1d',
+    redirect: false,
+    setHeaders: function (res, path, stat) {
+      res.set('x-timestamp', Date.now())
+    }
+  }
+
 const app = express();
 
 //Serve only the static files from the dist directory
-app.use(express.static('./dist/tienda-celulares-frontendapp'));
+app.use(express.static('./dist/tienda-celulares-frontendapp', options));
 
-app.use(express.static('./dist/tienda-celulares-frontendapp/success.html'));
-
-//app.use('/success.html', express.static('./dist/tienda-celulares-frontendapp/success.html'));
-
-app.get('/',(req, res) => 
-    res.sendFile('index.html', {root: 'dist/angular-heroku/'}),
+app.get('/success',(req, res) => 
+    res.sendFile('success.html'),
 );
 
-app.get('/Success',(req, res) => 
-    res.sendFile('success.html', {root: 'dist/angular-heroku/'}),
+app.get('/*',(req, res) => 
+    res.sendFile('index.html'),
 );
-
-app.get('/success.html',(req, res) => 
-    res.sendFile('./success.html', {root: 'dist/angular-heroku/'}),
-);
-
-app.get('/index.html',(req, res) => 
-    res.sendFile('index.html', {root: 'dist/angular-heroku/'}),
-);
-
-
-app.get('/Fail',(req, res) => 
-    res.sendFile('fail.html', {root: 'dist/angular-heroku/'}),
-);
-
-app.get('/Pending',(req, res) => 
-    res.sendFile('pending.html', {root: 'dist/angular-heroku/'}),
-);
-
 
 //Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
